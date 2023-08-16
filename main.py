6d2272497043bbbc09ac932bd9501d4a83c90ada
suyogkad <7suyog@gmail.com>
2023-08-16 14:00:28 +0545

    Project Added

---
+++ main.py
@@ -0,0 +1,7 @@
+from motion_detection import detect_motion
+
+# Define the path to your video
+video_path = "sample1.mp4"
+
+# Call the motion detection function on your video
+detect_motion(video_path)
