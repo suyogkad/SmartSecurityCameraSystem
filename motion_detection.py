fc58ba5982b39bbc64ed646edf1afecd8b4a46bf
suyogkad <7suyog@gmail.com>
2023-08-17 23:06:37 +0545

    Model Optimization For Detection Improvements

--- motion_detection.py
+++ motion_detection.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1e4fff2168fca3b13643df595d9756144212d4565dfadb0db52b00112c89998
-size 10946
+oid sha256:f44fa98c6c692466cdd798b205423dd6bc6e26683ba87a2be0321ad410d9ede0
+size 10967
